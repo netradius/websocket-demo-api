61ce0e3ee62552501832c1674ee7af42416d527e
Kevin Hawkins <khawkins@netradius.com>
2017-03-09 11:18:02 -0700

    Added template example.

--- src/main/java/com/netradius/controller/WebSocketDemoController.java
+++ src/main/java/com/netradius/controller/WebSocketDemoController.java
@@ -2,13 +2,19 @@ package com.netradius.controller;
 
 import com.netradius.data.dto.form.MessageRequest;
 import com.netradius.data.dto.view.MessageView;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Controller for request.
@@ -17,6 +23,9 @@ import java.util.Date;
  */
 @Controller
 public class WebSocketDemoController {
+
+	@Autowired
+	private SimpMessagingTemplate template;
 
 	@MessageMapping(value = "/add")
 	@SendTo("/topic/messages")
@@ -28,5 +37,30 @@ public class WebSocketDemoController {
 		messageView.setUsername(request.getUsername());
 		messageView.setDateTime(new Date());
 		return messageView;
+	}
+
+
+	//This is an example of using the simple messaging template to send websocket responses to subscribed clients
+	// from a normal http ajax request
+	@CrossOrigin
+	@RequestMapping(value = "/ping", method = RequestMethod.POST)
+	@ResponseBody
+	public void pingUsers(@RequestBody MessageRequest request) {
+		MessageView messageView = new MessageView();
+		messageView.setMessage(generateMeme());
+		messageView.setDateTime(new Date());
+		messageView.setUsername(request.getUsername());
+		template.convertAndSend("/topic/messages", messageView);
+	}
+
+	private String generateMeme() {
+		List<String> memes = new ArrayList<>();
+		memes.add("Cash me on da outside how bout dah!");
+		memes.add("Thug life!");
+		memes.add("#totes");
+		memes.add("YOLO!");
+		memes.add("...said no one ever!");
+		memes.add("Baller!");
+		return memes.get((int) Math.floor(Math.random() * memes.size()));
 	}
 }
